19669f591fcb0170fee9a1d45d4b71a2fe239b0b
Gerwin15 <gerwin_cando@rcc.edu.ph>
2024-09-14 12:09:27 +0800

    inputvalue

---
+++ src/InputComponent.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import "./AddNumbers.css";
+class InputComponent extends Component {
+
+    constructor(props) {
+    super(props);
+    this.state = {
+    inputValue:"",
+    }
+}
+    handleInputChange=(event)=>{
+    this.setState({
+        inputValue: event.target.value,
+    });
+    };
+
+
+render(){
+    return(
+        <div>
+            <h2>
+                Input Box Value
+            </h2>
+            <input 
+                type = "text"
+                placeholder='Enter Something...'
+                value={this.state.inputValue}
+                onChange={this.handleInputChange}
+            />
+            <p>You typed: {this.state.inputValue}</p>
+        </div>
+    );
+}
+
+}
+export default InputComponent;
